0b60673d9ac0c469a389c33398067f4d163e6b93
Jakke Lehtonen <jakke.lehtonen@gmail.com>
2025-05-26 20:22:06 +0300

    Add CSP example

--- wordpress_report.php
+++ wordpress_report.php
@@ -4,3 +4,9 @@
 add_action('send_headers', function() {
     header("Content-Security-Policy-Report-Only: default-src 'self'; ... ; report-uri https://csp.example.com/csp-violation");
 });
+
+# An example from one WordPress
+
+#add_action('send_headers', function() {
+#    header("Content-Security-Policy-Report-Only: default-src 'self'; script-src 'self' https://www.youtube.com https://*.googlesyndication.com https://*.google-analytics.com https://*.gstatic.com https://*.googleapis.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:; frame-src https://www.youtube.com; connect-src 'self'; report-uri https://csp.example.com/");
+#});
